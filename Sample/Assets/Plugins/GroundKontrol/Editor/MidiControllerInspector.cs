492ece609aaf52c2cf0aa45404a8f70478e7f428
Mike Lazer-Walker <michael@lazerwalker.com>
2019-03-13 11:44:29 -0500

    Add version of GroundKontrol to sample project

---
+++ Sample/Assets/Plugins/GroundKontrol/Editor/MidiControllerInspector.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json.Linq;
+using UnityEditor;
+using UnityEditor.UI;
+using UnityEngine;
+
+internal class SelectedItem
+{
+  public GameObject Object;
+  public int ComponentIndex = 0;
+  public int MemberIndex = 0;
+
+  public List<string> Components;
+  public List<string> Members;
+
+  public readonly InputConfiguration Item;
+
+  public bool Open = true;
+
+  public SelectedItem(MidiInput input, GameObject obj, List<string> components)
+  {
+    Item = new InputConfiguration(input);
+    Object = obj;
+    Components = components;
+
+    Members = obj == null ? new List<string>() : GetMembers(Object.GetComponent(Components[0])); 
+  }
+
+  public SelectedItem(InputConfiguration config, GameObject obj, List<string> components)
+  {
+    Item = config;
+    Object = obj;
+
+    Components = components;
+    Members = GetMembers(Object.GetComponent(config.Component));
+
+    ComponentIndex = components.IndexOf(Item.Component);
+    MemberIndex = Members.IndexOf(Item.Member);
+  }
+
+  public void SetComponentIndex(int index)
+  {
+    ComponentIndex = index;
+    MemberIndex = 0;
+    var componentName = Components.ElementAt(ComponentIndex);
+    Item.Component = componentName;
+
+    Members = GetMembers(Object.GetComponent(componentName));
+  }
+
+  private static List<string> GetMembers(Component component)
+  {
+    if (!component)
+    {
+      return new List<string>();
+    }
+    // We care about both properties and fields, so we grab them separately
+
+    var properties = component.GetType().GetProperties(System.Reflection.BindingFlags.Public |
+                                                       System.Reflection.BindingFlags.Instance |
+                                                       System.Reflection.BindingFlags.DeclaredOnly
+        )
+        .Where(o => _isNumber(o.PropertyType));
+
+    var fields = component.GetType().GetFields(System.Reflection.BindingFlags.Public |
+                                               System.Reflection.BindingFlags.Instance |
+                                               System.Reflection.BindingFlags.DeclaredOnly
+        )
+        .Where(o => _isNumber(o.FieldType));
+
+    return fields.Concat<MemberInfo>(properties)
+        .Select((m) => m.Name)
+        .ToList();
+  }
+
+  private static bool _isNumber(Type t)
+  {
+    return t.IsPrimitive &&
+           !(t == typeof(string) || t == typeof(bool) || t == typeof(char));
+  }
+}
+
+[CustomEditor(typeof(MidiController))]
+public class MidiControllerInspector : Editor
+{
+  private readonly List<SelectedItem> _items = new List<SelectedItem>();
+
+  private int _selectedInput = 0;
+
+  private List<string> _components = new List<string>();
+
+  private readonly List<MidiInput> _allInputs = MidiController.AllInputs();
+
+  private void OnEnable()
+  {
+    var t = (MidiController)target;
+
+    _components = t.gameObject.GetComponents<Component>()
+        .Select((m) => m.GetType().Name)
+        .ToList();
+
+    t.Inputs.ForEach((i) =>
+    {
+      var component = t.GetComponent(i.Component);
+      _items.Add(new SelectedItem(i, t.gameObject, _components));
+    });
+  }
+
+  public override void OnInspectorGUI()
+  {
+    foreach (var item in _items)
+    {
+      LayoutItem(item);
+    }
+
+    PromptForNewLayoutItem();
+
+    ((MidiController)target).Inputs = _items.Select((i) => i.Item).ToList();
+  }
+
+  private void LayoutItem(SelectedItem item)
+  {
+    item.Open = (EditorGUILayout.Foldout(item.Open, item.Item.Input.Name));
+    if (!item.Open) return;
+
+    EditorGUILayout.BeginVertical();
+
+    var componentIndex = EditorGUILayout.Popup("Component", item.ComponentIndex, item.Components.ToArray(), EditorStyles.popup);
+    if (componentIndex != item.ComponentIndex)
+    {
+      item.SetComponentIndex(componentIndex);
+    }
+
+    // TODO: Buttons as boolean toggles?
+
+    if (item.Members.Count > 0)
+    {
+      item.MemberIndex =
+          EditorGUILayout.Popup("Property", item.MemberIndex, item.Members.ToArray(), EditorStyles.popup);
+      item.Item.Member = item.Members.ElementAt(item.MemberIndex);
+
+      item.Item.Range = EditorGUILayout.DelayedIntField("Range", item.Item.Range);
+    }
+
+    if (GUILayout.Button("Remove"))
+    {
+      // TODO: Lol, don't remove while enumerating
+      _items.Remove(item);
+    }
+
+    EditorGUILayout.EndVertical();
+  }
+
+  private void PromptForNewLayoutItem()
+  {
+    var inputs = _availableInputs();
+    EditorGUI.BeginDisabledGroup(inputs.Count == 0);
+
+    var strings = _inputsToStringList(inputs);
+
+    EditorGUILayout.Separator();
+
+    _selectedInput = EditorGUILayout.Popup("Add New Mapping", 0, strings, EditorStyles.popup);
+
+    if (_selectedInput == 0) return;
+
+    var input = inputs.ElementAt(_selectedInput - 1);
+    _items.Add(new SelectedItem(input, ((MidiController)target).gameObject, _components));
+    _selectedInput = 0;
+
+    EditorGUI.EndDisabledGroup();
+  }
+
+  private List<MidiInput> _availableInputs()
+  {
+    // TODO: This is no longer working as expected
+    var currentInputs = _items.Select((i) => i.Item.Input);
+    var all = _allInputs.Except(currentInputs).ToList();
+    return all.ToList();
+  }
+
+  private static string[] _inputsToStringList(IEnumerable<MidiInput> list)
+  {
+    var strings = list.Select(i => i.Name).ToList();
+    strings.Insert(0, "Select an Input");
+    return strings.ToArray();
+  }
+}
---
+++ Sample/Assets/Plugins/GroundKontrol/MidiController.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using MidiJack;
+using UnityEngine;
+
+[Serializable]
+public enum MidiInputType
+{
+	Slider,
+	Knob
+}
+
+[Serializable]
+public class MidiInput
+{
+	[SerializeField]
+	public MidiInputType Type;
+	
+	[SerializeField]
+	public int Number;
+
+	public MidiInput(MidiInputType type, int number)
+	{
+		Type = type;
+		Number = number;
+	}
+
+	public int KnobNumber
+	{
+		get
+		{
+			switch (Type)
+			{
+				case MidiInputType.Knob:
+					return Number + 16;
+				case MidiInputType.Slider:
+					return Number;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+	}
+
+	public string Name => $"{Type} {Number + 1}";
+}
+
+[Serializable]
+public class InputConfiguration
+{
+	public string Component;
+	public string Member;
+	
+	// TODO: Range as a min/max instead of just a max?
+	public int Range = 1;
+	
+	[SerializeField]
+	public MidiInput Input;
+
+	public InputConfiguration(MidiInput input)
+	{
+		Debug.Log("Creating INputConfiguration with MidiInput");
+		Input = input;
+	}
+	
+	public InputConfiguration(string component, string member, int range, MidiInput input)
+	{
+		Component = component;
+		Member = member;
+		Range = range;
+		Input = input;
+	}
+}
+
+public class MidiController : MonoBehaviour
+{
+	private readonly Dictionary<float, float> _previousValues = new Dictionary<float, float>();
+	
+	public List<InputConfiguration> Inputs = new List<InputConfiguration>();
+
+	public static List<MidiInput> AllInputs()
+	{
+		var list = new List<MidiInput>();
+		for (var i = 0; i < 8; i++)
+		{
+			list.Add(new MidiInput(MidiInputType.Knob, i));
+		}
+		
+		for (var i = 0; i < 8; i++)
+		{
+			list.Add(new MidiInput(MidiInputType.Slider, i));
+		}
+
+		return list;
+	}
+
+	void Update()
+	{
+		var shouldChange = !(MidiMaster.GetKnob(MidiChannel.All, 43) > 0.0);
+
+		Inputs.ForEach((i) => UpdateValue(i, shouldChange));
+	}
+
+	private void UpdateValue(InputConfiguration i, bool shouldChange)
+	{
+		if (i.Component == null || i.Member == null)
+		{
+			return;
+		}
+		
+		// TODO: Cache these lookups from update to update?
+		var component = GetComponent(i.Component);
+		var type = component.GetType();
+		MemberInfo member = type.GetField(i.Member);
+		
+		if (member == null)
+		{
+			member = type.GetProperty(i.Member);
+		}
+
+		if (member == null)
+		{
+			Debug.Log("No member! Returning!");
+			return;
+		}
+
+        
+		var previousValue = 0.0f;
+		if (_previousValues.ContainsKey(i.Input.KnobNumber))
+		{
+			previousValue = _previousValues[i.Input.KnobNumber];
+		}
+
+		var knobValue = MidiMaster.GetKnob(MidiChannel.All, i.Input.KnobNumber) * i.Range;
+		var difference = knobValue - previousValue;
+		var newValue = (float) GetValue(member, component) + difference;
+
+		_previousValues[i.Input.KnobNumber] = knobValue;
+		
+		if (shouldChange)
+		{
+			SetValue(member, component, newValue);
+		}
+	}
+	
+	// Via https://stackoverflow.com/questions/12680341/how-to-get-both-fields-and-properties-in-single-call-via-reflection
+	public static void SetValue(MemberInfo member, object owner, object value)
+	{
+		switch (member.MemberType)
+		{
+			case MemberTypes.Property:
+				((PropertyInfo)member).SetValue(owner, value, null);
+				break;
+			case MemberTypes.Field:
+				((FieldInfo)member).SetValue(owner, value);
+				break;
+			default:
+				throw new Exception("Property must be of type FieldInfo or PropertyInfo");
+		}
+	}
+
+	public static object GetValue(MemberInfo member, object owner)
+	{
+		switch (member.MemberType)
+		{
+			case MemberTypes.Property:
+				return ((PropertyInfo)member).GetValue(owner, null);
+			case MemberTypes.Field:
+				return ((FieldInfo)member).GetValue(owner);
+			default:
+				throw new Exception("Property must be of type FieldInfo or PropertyInfo");
+		}
+	}
+
+	public static object GetValue(InputConfiguration input, GameObject owner)
+	{
+		var component = owner.GetComponent(input.Component);
+		var type = component.GetType();
+		MemberInfo member = type.GetField(input.Member);
+		
+		if (member == null)
+		{
+			member = type.GetProperty(input.Member);
+		}
+
+		if (member == null)
+		{
+			Debug.Log("No member! Returning!");
+			return null;
+		}
+
+		return GetValue(member, component);
+	}
+
+	private static Type GetType(MemberInfo member)
+	{
+		switch (member.MemberType)
+		{
+			case MemberTypes.Field:
+				return ((FieldInfo)member).FieldType;
+			case MemberTypes.Property:
+				return ((PropertyInfo)member).PropertyType;
+			case MemberTypes.Event:
+				return ((EventInfo)member).EventHandlerType;
+			default:
+				throw new ArgumentException("MemberInfo must be if type FieldInfo, PropertyInfo or EventInfo", nameof(member));
+		}
+	}
+}
